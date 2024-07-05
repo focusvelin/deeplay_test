d34115491d179d5027b897ef85fb7a46b1b6ee83
focusvelin <focusvelin@yandex.ru>
2024-07-05 13:32:35 +0000

    New version of rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-dataDir="mongodb_data"
-
-echo "### Deleting containers ###"
-docker-compose down --volume
-
-if [ $? -ne 0 ]
-then
-  echo "### Error while stopping containers ###"
-  exit 1
-fi
-
-#Removing data in dataDir
-rm -rf $dataDir/*
-
-echo "### Starting containers ###"
+echo "### Starting rebuild!!! ###"
+docker-compose down --volumes
 docker-compose up -d
 
 if [ $? -ne 0 ]
 then
-  echo "### Error while starting containers ###"
+  echo "### Error while starting containers!!! ###"
   exit 1
 fi
 
-echo "### Mongodb rebuild has been successfuly completed ###"
+echo "### Mongodb rebuild has been successfuly completed!!! ###"
