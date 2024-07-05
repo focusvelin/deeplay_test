2fb9225ff84cff65458fca536d9da882dbdff9e0
focusvelin <focusvelin@yandex.ru>
2024-07-05 10:18:19 +0000

    New version of backup.sh that uses mongodump utility

--- backup.sh
+++ backup.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-backupPath="backup/"
-dataPath="mongodb_data/"
-
-#Checking mongo data existence, if not then nothing to backup
-if [[ ! -d $dataPath ]]
+#Checking if .env file exists
+if [ ! -f .env ]
 then
-  echo "### No data to backup!!! ###"
+  echo "### .env file doesn't exist!!! You need to create one in order to pass credentials to the script ###"
   exit 1
 fi
 
-#Checking backup directory existence, if not then create one
-if [[ ! -d $backupPath ]]
+#Exporting env variables
+export $(cat .env)
+
+#Defining variables
+CONTAINER_NAME="mongodb"
+BACKUP_DIR="./backup"
+USER=$MONGO_USER
+PASSWORD=$MONGO_PASSWORD
+
+#Checking if container exists and running
+if [ -z `docker-compose ps -q $CONTAINER_NAME` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $CONTAINER_NAME)` ]
 then
-  mkdir $backupPath
+  echo "### Mongodb container is not running ###"
+  exit 1
 fi
 
-echo "### Stopping containers ###"
-docker-compose stop
+#Create backup directory if doesn't exist
+mkdir -p $BACKUP_DIR
 
 echo "### Starting backup ###"
-tar -czvf ${backupPath}data_$(date +%Y%m%d)_$(date +%H%M).tar.gz ${dataPath}
+docker exec $CONTAINER_NAME mongodump --username $USER --password $PASSWORD --archive > $BACKUP_DIR/data_$(date +%Y%m%d)_$(date +%H%M).gz
 
 if [ $? -ne 0 ]
 then
   echo "### Error while backup ###"
-  exit 1
-fi
-
-echo "### Starting containers ###"
-docker-compose start
-
-if [ $? -ne 0 ]
-then
-  echo "### Error while starting containers ###"
   exit 1
 fi
 
