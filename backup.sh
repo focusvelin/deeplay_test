86420cff94d506139ae14836eb68a89fc52d13e1
focusvelin <focusvelin@yandex.ru>
2024-07-07 16:38:30 +0000

    add brackets to variables in scripts

--- backup.sh
+++ backup.sh
@@ -13,21 +13,21 @@ export $(cat .env)
 #Defining variables
 CONTAINER_NAME="mongodb"
 BACKUP_DIR="./backup"
-USER=$MONGO_USER
-PASSWORD=$MONGO_PASSWORD
+USER=${MONGO_USER}
+PASSWORD=${MONGO_PASSWORD}
 
 #Checking if container exists and running
-if [ -z `docker-compose ps -q $CONTAINER_NAME` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $CONTAINER_NAME)` ]
+if [ -z `docker-compose ps -q ${CONTAINER_NAME}` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q ${CONTAINER_NAME})` ]
 then
   echo "### Mongodb container is not running ###"
   exit 1
 fi
 
 #Create backup directory if doesn't exist
-mkdir -p $BACKUP_DIR
+mkdir -p ${BACKUP_DIR}
 
 echo "### Starting backup ###"
-docker exec $CONTAINER_NAME mongodump --username $USER --password $PASSWORD --archive > $BACKUP_DIR/data_$(date +%Y%m%d)_$(date +%H%M).gz
+docker exec ${CONTAINER_NAME} mongodump --username ${USER} --password ${PASSWORD} --archive > ${BACKUP_DIR}/data_$(date +%Y%m%d)_$(date +%H%M).gz
 
 if [ $? -ne 0 ]
 then
--- restore.sh
+++ restore.sh
@@ -13,31 +13,31 @@ export $(cat .env)
 #Defining variables
 CONTAINER_NAME="mongodb"
 BACKUP_FILE=$1
-USER=$MONGO_USER
-PASSWORD=$MONGO_PASSWORD
+USER=${MONGO_USER}
+PASSWORD=${MONGO_PASSWORD}
 
 #Checking if a backup file is provided
-if [ -z "$BACKUP_FILE" ]
+if [ -z "${BACKUP_FILE}" ]
 then
   echo "### No backup file path was provided!!! ###"
   exit 1
 fi
 
 #Checking if backup file exists
-if [ ! -f "$BACKUP_FILE" ]
+if [ ! -f "${BACKUP_FILE}" ]
 then
   echo "### Provided backup file doesn't exists!!! ###"
 fi
 
 #Checking if container exists and running
-if [ -z `docker-compose ps -q $CONTAINER_NAME` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $CONTAINER_NAME)` ]
+if [ -z `docker-compose ps -q ${CONTAINER_NAME}` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q ${CONTAINER_NAME})` ]
 then
   echo "### Mongodb container is not running!!! ###"
   exit 1
 fi
 
 echo "### Starting data restore ###"
-docker exec -i $CONTAINER_NAME mongorestore --username $USER --password $PASSWORD --archive < $BACKUP_FILE
+docker exec -i ${CONTAINER_NAME} mongorestore --username ${USER} --password ${PASSWORD} --archive < ${BACKUP_FILE}
 
 if [ $? -ne 0 ]
 then
